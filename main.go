9b73e32e436a02a60b3f6fc4aae55756027c3f42
sayed belal <sayed.b@greyorange.com>
2023-04-10 22:35:51 +0530

    adding the main file with read and insert

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Notes struct {
+	Id        int
+	Noteslist string
+}
+
+func main() {
+
+	db, err := sql.Open("mysql", "root:notes@(127.0.0.1:13306)/notes")
+
+	defer db.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := db.Query("SELECT * FROM noted")
+
+	defer res.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for res.Next() {
+
+		var note Notes
+		err := res.Scan(&note.Id, &note.Noteslist)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%v\n", note)
+	}
+
+	sql := "insert into noted (ID, Noteslist) values (3, 'This is my fourth note from GOapp');"
+	res2, err := db.Exec(sql)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	lastId, err := res2.LastInsertId()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("The last inserted row id: %d\n", lastId)
+
+}
